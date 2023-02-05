9927dae0319c90070eec6c0ffc426d24be0a8d4f
Sergey <6ypka@mail.ru>
2023-02-05 20:09:37 +0300

    HomeWork

---
+++ Task38/Program.cs
@@ -0,0 +1,45 @@
+﻿/* Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементами массива.
+
+[3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2 */
+
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[size];
+Array( array);
+Console.WriteLine();
+PrintArray( array);
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int i = 0; i <  array.Length; i++)
+{
+    if ( array[i] > max)
+        {
+            max =  array[i];
+        }
+    if ( array[i] < min)
+        {
+            min =  array[i];
+        }
+}
+void Array(double[]  array)
+{
+    for(int i = 0; i <  array.Length; i++)
+        {
+              array[i] =  Convert.ToDouble(new Random().Next(100,10000)) / 100;
+             // есть ли более простой способ, чтобы в 38 сроке сократить код? а просто вывести  array[i]
+        }
+}
+void PrintArray(double[]  array)
+{
+    Console.Write("|");
+    for(int i = 0; i <  array.Length; i++)
+        {
+            Console.Write($"{Math.Round( array[i], 1, MidpointRounding.ToZero)}|");
+        }
+    
+    Console.WriteLine();
+}
+ 
+
+Console.WriteLine($"Разница между максимальным и минимальным значением  массива = {Math.Round(max-min, 1, MidpointRounding.ToZero)}");
---
+++ Task41/Program.cs
@@ -0,0 +1,32 @@
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+-1, -7, 567, 89, 223-> 3 */
+
+Console.Write($"Количество чисел для ввода: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[m];
+
+void Input(int m){
+for (int i = 0; i < m; i++)
+  {
+    Console.Write($"Введи {i+1} число: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+  }
+}
+
+
+int Exam(int[] array)
+{
+  int count = 0;
+  for (int i = 0; i < array.Length; i++)
+  {
+    if(array[i] > 0 ) count += 1; 
+  }
+  return count;
+}
+
+Input(m);
+
+Console.WriteLine($"Чисел больше 0: {Exam(array)} ");
